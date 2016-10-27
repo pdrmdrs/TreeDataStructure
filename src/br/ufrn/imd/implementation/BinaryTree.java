eaf29d32a9e0fcb7bb75ccc8ac5ea6ae93cbdb2f
Pedro Paulo Paiva de Medeiros <pdr_mdrs@hotmail.com>
2016-10-26 23:55:36 -0300

    Rearranged the project packages Implemented search and insert methods on the binary tree

--- src/br/ufrn/imd/implementation/BinaryTree.java
+++ src/br/ufrn/imd/implementation/BinaryTree.java
@@ -1,5 +1,73 @@
 package br.ufrn.imd.implementation;
 
-public class BinaryTree {
+import br.ufrn.imd.domain.Node;
+
+public class BinaryTree<T extends Comparable<T>> {
+
+	private Node<T> root;
+	private int size;
+
+	public BinaryTree() {
+		this.root = null;
+	}
+
+	public BinaryTree(T rootValue) {
+		this.root = new Node<T>(rootValue);
+	}
+
+	public Node<?> getRoot() {
+		return root;
+	}
+
+	public void setRoot(Node<T> root) {
+		this.root = root;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public void insert(T value) {
+		this.root = insert(this.root, value);
+	}
+
+	private Node<T> insert(Node<T> node, T value) {
+		if (node == null) {
+			return new Node<T>(value);
+		}
+		if (node.getValue().compareTo(value) == 0) {
+			return node;
+		}
+		if (node.getValue().compareTo(value) > 0) {
+			node.setLeft(insert(node.getLeft(), value));
+		} else {
+			node.setRight(insert(node.getRight(), value));
+		}
+		return node;
+	}
+
+	public void delete(Node<T> node) {
+
+	}
+
+	public boolean search(T value) {
+		return search(this.root, value);
+	}
+
+	private boolean search(Node<T> node, T value) {
+		if (node == null) {
+			return false;
+		} else if (node.getValue().compareTo(value) == 0) {
+			return true;
+		} else if (node.getValue().compareTo(value) > 0) {
+			return search(node.getLeft(), value);
+		} else {
+			return search(node.getRight(), value);
+		}
+	}
 
 }
--- src/br/ufrn/imd/main/Main.java
+++ src/br/ufrn/imd/main/Main.java
@@ -1,10 +1,29 @@
 package br.ufrn.imd.main;
 
+import br.ufrn.imd.implementation.BinaryTree;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
+
+		Integer[] intArray = { 1, 5, 2, 7, 4 };
+
+		BinaryTree<Integer> bt = new BinaryTree<Integer>();
+
+		for (Integer n : intArray)
+			bt.insert(n);
+
+		System.out.println(bt.getRoot().getValue());// 1
+		System.out.println(bt.getRoot().getRight().getValue());// 5
+		System.out.println(bt.getRoot().getRight().getLeft().getValue());// 2
+		System.out.println(bt.getRoot().getRight().getRight().getValue());// 7
+		System.out.println(bt.getRoot().getRight().getLeft().getRight().getValue());// 4
+		System.out.println("------");
+		System.out.println(bt.search(4));//true
+		System.out.println(bt.search(3));//false
+		System.out.println("------");
+		
 	}
 
 }
