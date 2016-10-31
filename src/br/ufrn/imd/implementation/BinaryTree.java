bb3a2b9efb10b4f3419d4efc13aefceba0aebbed
Pedro Paulo Paiva de Medeiros <pdr_mdrs@hotmail.com>
2016-10-30 23:03:31 -0300

    Finished implementation of BinaryTree
    Finished the implementation of the BinarySearchTree
    Added the Exceptions package and the CannotDeleteException

--- src/br/ufrn/imd/implementation/BinaryTree.java
+++ src/br/ufrn/imd/implementation/BinaryTree.java
@@ -1,6 +1,7 @@
 package br.ufrn.imd.implementation;
 
 import br.ufrn.imd.domain.Node;
+import br.ufrn.imd.exceptions.CannotDeleteException;
 
 public class BinaryTree<T extends Comparable<T>> {
 
@@ -33,6 +34,7 @@ public class BinaryTree<T extends Comparable<T>> {
 
 	public void insert(T value) {
 		this.root = insert(this.root, value);
+		this.size+= 1;
 	}
 
 	private Node<T> insert(Node<T> node, T value) {
@@ -50,8 +52,37 @@ public class BinaryTree<T extends Comparable<T>> {
 		return node;
 	}
 
-	public void delete(Node<T> node) {
+	public void delete(T value) throws CannotDeleteException {
+		this.root = delete(this.root, value);
+	}
+	
+	private Node<T> delete(Node<T> node, T value) throws CannotDeleteException {
+		if (node == null) {
+			throw new CannotDeleteException("Cannot delete this item");
+		} else if(node.getValue().compareTo(value) > 0) {
+			node.setLeft(delete(node.getLeft(), value));
+		} else if(node.getValue().compareTo(value) < 0) {
+			node.setRight(delete(node.getRight(), value));
+		} else {
+			if(node.getLeft() == null) {
+				return node.getRight();
+			} else if(node.getRight() == null){
+				return node.getLeft();
+			} else {
+				this.size -= 1;
+				node.setValue(getData(node.getLeft()));
+				node.setLeft(delete(node.getLeft(), node.getValue()));
+			}
+		}
+		
+		return null;
+	}
 
+	private T getData(Node<T> node) {
+		while (node.getRight() != null) {
+			node = node.getRight();
+		}
+		return node.getValue();
 	}
 
 	public boolean search(T value) {
--- src/br/ufrn/imd/main/Main.java
+++ src/br/ufrn/imd/main/Main.java
@@ -1,29 +1,9 @@
 package br.ufrn.imd.main;
 
-import br.ufrn.imd.implementation.BinaryTree;
-
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-
-		Integer[] intArray = { 1, 5, 2, 7, 4 };
-
-		BinaryTree<Integer> bt = new BinaryTree<Integer>();
-
-		for (Integer n : intArray)
-			bt.insert(n);
-
-		System.out.println(bt.getRoot().getValue());// 1
-		System.out.println(bt.getRoot().getRight().getValue());// 5
-		System.out.println(bt.getRoot().getRight().getLeft().getValue());// 2
-		System.out.println(bt.getRoot().getRight().getRight().getValue());// 7
-		System.out.println(bt.getRoot().getRight().getLeft().getRight().getValue());// 4
-		System.out.println("------");
-		System.out.println(bt.search(4));//true
-		System.out.println(bt.search(3));//false
-		System.out.println("------");
-		
 	}
 
 }
