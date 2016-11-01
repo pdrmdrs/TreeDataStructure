a3b1ac270924eca776fbbee1f329e86f754585f4
Pedro Paulo Paiva de Medeiros <pdr_mdrs@hotmail.com>
2016-11-01 00:07:34 -0300

    Finalized the Tree Implementation
    Finalized the Tree implementation
    Added the documentation of the code
    Added the BinaryTree test class
    Added the JAVADoc of the project

--- src/br/ufrn/imd/implementation/BinaryTree.java
+++ src/br/ufrn/imd/implementation/BinaryTree.java
@@ -3,38 +3,91 @@ package br.ufrn.imd.implementation;
 import br.ufrn.imd.domain.Node;
 import br.ufrn.imd.exceptions.CannotDeleteException;
 
+/**
+ * Class that represents the Binary Search Tree
+ * 
+ * @author Pedro Paulo Paiva de Medeiros
+ *
+ * @param <T>
+ *            the type of the itens that the tree will contain
+ */
 public class BinaryTree<T extends Comparable<T>> {
 
+	/**
+	 * The root node of the tree
+	 */
 	private Node<T> root;
-	private int size;
 
+	/**
+	 * The number of the elements on the tree
+	 */
+	private int size = 1;
+
+	/**
+	 * Empty constructor
+	 */
 	public BinaryTree() {
 		this.root = null;
 	}
 
+	/**
+	 * The constructor of the tree receiving the root value
+	 * 
+	 * @param rootValue
+	 *            the value of the root
+	 */
 	public BinaryTree(T rootValue) {
 		this.root = new Node<T>(rootValue);
 	}
 
+	/**
+	 * Get the root node
+	 * 
+	 * @return the root node
+	 */
 	public Node<?> getRoot() {
 		return root;
 	}
 
+	/**
+	 * Set the node of the root
+	 * 
+	 * @param root
+	 *            to the root of the tree
+	 */
 	public void setRoot(Node<T> root) {
 		this.root = root;
 	}
 
+	/**
+	 * Get the number of elements on the tree
+	 * 
+	 * @return the size of the tree
+	 */
 	public int getSize() {
 		return size;
 	}
 
+	/**
+	 * Set the size of the tree (Only here to contemplate the "getters and
+	 * setters for all atributes")
+	 * 
+	 * @param size
+	 *            to the size of the tree
+	 */
 	public void setSize(int size) {
 		this.size = size;
 	}
 
+	/**
+	 * Insert a value on the tree
+	 * 
+	 * @param value
+	 *            to insert on the tree
+	 */
 	public void insert(T value) {
 		this.root = insert(this.root, value);
-		this.size+= 1;
+		this.size += 1;
 	}
 
 	private Node<T> insert(Node<T> node, T value) {
@@ -52,21 +105,29 @@ public class BinaryTree<T extends Comparable<T>> {
 		return node;
 	}
 
+	/**
+	 * Delete a value on the tree
+	 * 
+	 * @param value
+	 *            to delete on the tree
+	 * @throws CannotDeleteException
+	 *             exception for when the item cant be deleted
+	 */
 	public void delete(T value) throws CannotDeleteException {
 		this.root = delete(this.root, value);
 	}
-	
+
 	private Node<T> delete(Node<T> node, T value) throws CannotDeleteException {
 		if (node == null) {
 			throw new CannotDeleteException("Cannot delete this item");
-		} else if(node.getValue().compareTo(value) > 0) {
+		} else if (node.getValue().compareTo(value) > 0) {
 			node.setLeft(delete(node.getLeft(), value));
-		} else if(node.getValue().compareTo(value) < 0) {
+		} else if (node.getValue().compareTo(value) < 0) {
 			node.setRight(delete(node.getRight(), value));
 		} else {
-			if(node.getLeft() == null) {
+			if (node.getLeft() == null) {
 				return node.getRight();
-			} else if(node.getRight() == null){
+			} else if (node.getRight() == null) {
 				return node.getLeft();
 			} else {
 				this.size -= 1;
@@ -74,7 +135,7 @@ public class BinaryTree<T extends Comparable<T>> {
 				node.setLeft(delete(node.getLeft(), node.getValue()));
 			}
 		}
-		
+
 		return null;
 	}
 
@@ -85,6 +146,13 @@ public class BinaryTree<T extends Comparable<T>> {
 		return node.getValue();
 	}
 
+	/**
+	 * Search if a value is present on the tree or not
+	 * 
+	 * @param value
+	 *            the value to search
+	 * @return if the value is present or not
+	 */
 	public boolean search(T value) {
 		return search(this.root, value);
 	}
